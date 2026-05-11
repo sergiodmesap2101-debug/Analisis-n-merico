15b3fb09edc20f77c26a20b4dd20d3d2d909caa2
sergioelpro <sergiodmesap2101@gmail.com>
2026-05-11 10:19:54 -0500

    Fix railway deploy

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ Ejecutar: python app.py
 from flask import Flask, render_template, jsonify, request
 from src.game import GameState
 
+import os
+
 app = Flask(__name__)
 game = None
 
@@ -47,4 +49,5 @@ def continuar():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
